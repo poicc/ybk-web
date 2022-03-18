45d3f873d491b8a39ec2c1df6dcb2539e5ba2b48
w2gd <w2gd@qq.com>
2022-03-18 14:43:26 +0800

    0.01

---
+++ src/components/card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <div>
+      <img src="../assets/logo.png" alt="">
+      <h2>Vue3</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="about">
+    <div>
+      <img src="../assets/logo.png" alt="">
+      <h2>Vue3</h2>
+    </div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.about {
+  background-color: white;
+  width: 980px;
+  margin: auto;
+  display: grid;
+  grid-template-columns: repeat(4, 220px);
+  gap: 20px 20px;
+  
+  padding: 20px 20px;
+  div {
+    background-color: cyan;
+    border: 1px solid black;
+    height: 319px;
+
+    img {
+      width: 100%;
+      height: 220px;
+    }
+
+
+  }
+}
+</style>
